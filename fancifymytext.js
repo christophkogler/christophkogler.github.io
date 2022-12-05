246edaadb5ce7e8cc4b22c3c97f8b55af7060ea9
Christoph Kogler <christophkogler@csus.edu>
2022-12-04 19:05:14 -0800

    FancifySchmancify

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,8 +1,7 @@
 //alert("Hello, world!");
 
 function bigger() {
-	var words = document.getElementByID("textbox");
-	words.style.fontSize="24pt";
+	document.getElementById("textbox").style.fontSize = "24px";
 	hello();
 }
 
@@ -11,13 +10,27 @@ function hello() {
 }
 
 function fancy() {
-	var words = document.getElementByID("textbox");
+	var words = document.getElementById("textbox");
 	words.style.fontSize="24pt";
 	words.style.fontWeight = "bold";
 	words.style.color = "red";
-	words.style.text-decoration = "underline";
+	words.style.backgroundColor = "black";
+	words.style.textDecoration = "underline";
 	hello();
 }
 	
 function boring() {
-	words.
+	var words = document.getElementById("textbox");
+	words.style.fontWeight = "normal";
+	words.style.color = "black";
+	words.style.textDecoration = "none";
+	words.style.backgroundColor = "white";
+	hello();
+}
+	
+function moo() {
+	var words = document.getElementById("textbox");
+	var splitting = words.value.split(".");
+	words.value = splitting.join("-Moo.");
+	hello();
+}
